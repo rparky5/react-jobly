f0a191a03ec1bd2fbb5c99006c6ad4b78c5a2059
Kenny Burgher <kenny.burgher@gmail.com>
2023-06-02 11:41:19 -0700

    useEffect for updating local storage

--- src/App.js
+++ src/App.js
@@ -7,47 +7,41 @@ import JoblyApi from "./api";
 import jwt_decode from "jwt-decode";
 import userContext from "./userContext";
 
-
 function App() {
   const [error, setError] = useState(null);
   const [user, setUser] = useState(null);
-  const [token, setToken] = useState(localStorage.getItem('token') || "");
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [isLoading, setIsLoading] = useState(token && !user);
 
-  useEffect(function getUserOnMountIfToken() {
-    async function getUserIfToken() {
-      JoblyApi.token = token;
-      const decoded = jwt_decode(token);
-      try {
-        const user = await JoblyApi.getUser(decoded.username);
-        setUser(user);
-        setIsLoading(false);
-      } catch (err) {
-        setError(err);
+  useEffect(
+    function updateLocalStorage() {
+      token ? localStorage.setItem("token", token) : localStorage.removeItem("token");
+    },
+    [token]
+  );
+
+  useEffect(
+    function getUserOnLoginOrSignup() {
+      async function getUser() {
+        JoblyApi.token = token;
+        const decoded = jwt_decode(token);
+        try {
+          const user = await JoblyApi.getUser(decoded.username);
+          setUser(user);
+          setIsLoading(false);
+        } catch (err) {
+          setError(err);
+        }
       }
-    }
-    if (token) getUserIfToken();
-  }, []);
 
-  useEffect(function getUserOnLoginOrSignup() {
-    async function getUser() {
-      const decoded = jwt_decode(token);
-      try {
-        const user = await JoblyApi.getUser(decoded.username);
-        setUser(user);
-        setIsLoading(false);
-      } catch (err) {
-        setError(err);
+      if (!token) {
+        setUser(null);
+      } else {
+        getUser();
       }
-    }
-
-    if (!token) {
-      setUser(null);
-    } else {
-      getUser();
-    }
-  }, [token]);
-
+    },
+    [token]
+  );
 
   async function login({ username, password }) {
     const newToken = await JoblyApi.login(username, password);
@@ -55,16 +49,14 @@ function App() {
     setIsLoading(true);
   }
 
-
   async function signup(user) {
     const signupRes = await JoblyApi.signup(user);
-    setToken(signupRes)
+    setToken(signupRes);
     setIsLoading(true);
   }
 
   function logout() {
-    setToken('');
-    localStorage.removeItem("token");
+    setToken("");
   }
 
   async function updateProfile(updatedData) {
@@ -72,20 +64,19 @@ function App() {
     setUser(updateRes);
   }
 
-
-  // TODO: dynamic errors
-  console.log('error', error)
-  if (error) return <Navigate to={`/404`} />;
-  if (token) localStorage.setItem("token", token);
-
-  if(isLoading) return <p>Loading...</p>
+  if (error) return <Navigate to={`/500`} />;
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <div className="App">
       <userContext.Provider value={user}>
         <BrowserRouter>
           <Nav logout={logout} />
-          <RoutesList login={login} signup={signup} updateProfile={updateProfile} />
+          <RoutesList
+            login={login}
+            signup={signup}
+            updateProfile={updateProfile}
+          />
         </BrowserRouter>
       </userContext.Provider>
     </div>
